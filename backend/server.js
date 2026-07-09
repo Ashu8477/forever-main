6995d02888d9ca22593ff75cfced31e085c0e86e
Ashu Singh <singhaashu271@gmail.com>
2026-07-09 23:35:37 +0530

    UPDATE

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config' 
+import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
@@ -26,4 +26,4 @@ app.get('/', (req, res) => {
     res.send("API working")
 })
 
-app.listen(port, ()=> console.log('Server started on port: ' + port));
+app.listen(port, ()=> console.log('Server started on portt: ' + port));
